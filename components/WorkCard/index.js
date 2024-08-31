bdcb606cc59120ac3ada50bb1421816e327e760d
Duongg <ducduong2201@gmail.com>
2024-08-31 10:48:04 +0700

    fix size image work card

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -7,19 +7,19 @@ const WorkCard = ({ img, name, description, onClick }) => {
       onClick={onClick}
     >
       <div
-        className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
-        style={{ height: "600px" }}
+        className="flex items-center justify-center h-screen rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
+        style={{ height: "300px" }}
       >
         <img
           alt={name}
-          className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
+          className="h-full w-3/5 rounded-lg object-cover hover:scale-110 transition-all ease-out duration-300"
           src={img}
         ></img>
       </div>
-      <h1 className="mt-5 text-3xl font-medium">
+      <h1 className="mt-5 text-2xl font-medium flex items-center justify-center">
         {name ? name : "Project Name"}
       </h1>
-      <h2 className="text-xl opacity-50">
+      <h2 className="text-xl opacity-50 flex items-center justify-center">
         {description ? description : "Description"}
       </h2>
     </div>
