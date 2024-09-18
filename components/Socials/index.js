03320d1a0e85268a1e0c8e7b56a586ebe328f499
Duongg <ducduong2201@gmail.com>
2024-09-18 10:09:27 +0700

    update image in app card, add resume in social

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -2,15 +2,24 @@ import React from "react";
 import Button from "../Button";
 
 import yourData from "../../data/portfolio.json";
+import { useRouter } from "next/router";
 
 const Socials = ({ className }) => {
+  const router = useRouter();
   return (
     <div className={`${className} flex flex-wrap mob:flex-nowrap link`}>
       {yourData.socials.map((social, index) => (
         <Button key={index} onClick={() => window.open(social.link)}>
           {social.title}
         </Button>
+
       ))}
+      <Button
+        onClick={() =>
+          router.push("/resume")
+        }>
+        Resume
+      </Button>
     </div>
   );
 };
